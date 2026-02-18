d571e3a0464a66a099389cc9764032c428273ef9
Wajiha Kouser <141769095+wajihawajid5610@users.noreply.github.com>
2026-02-18 10:28:59 +0500

    fix prisma vercel

--- app/utils/connection.ts
+++ app/utils/connection.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client"; 
+/* import { PrismaClient } from "../generated/prisma/client"; 
 
 
 
@@ -10,5 +10,23 @@ const globalForPrisma = globalThis as unknown as {
  const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
+ */
+
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+declare global {
+  var prisma: ReturnType<typeof prismaClientSingleton> | undefined
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
 export default prisma
